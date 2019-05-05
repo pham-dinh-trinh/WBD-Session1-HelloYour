ad9d4d22718791ab98661cddd6215072c7dc85ae
pham-dinh-trinh <phamdinhtrinhcb27@gmail.com>
2019-05-05 14:04:51 +0700

    Add Hello Your

---
+++ src/HelloYour.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class HelloYour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What your name : ");
+        String name = scanner.nextLine();
+        System.out.println("Hello : " + name);
+    }
+}
